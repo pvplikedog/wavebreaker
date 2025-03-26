390b0a9708750247e5c59bff4e5751003cd0421b
Anton Anohin <pvplikedog@gmail.com>
2025-03-26 22:34:17 +0300

    feat: :sparkles: add circle melee weapon

--- Assets/Scripts/Weapon/MeleeCircleWeapon.cs
+++ Assets/Scripts/Weapon/MeleeCircleWeapon.cs
@@ -4,7 +4,15 @@ public class MeleeCircleWeapon : Weapon
 {
     [SerializeField] private float range = 2.5f;
     [SerializeField] private float damage = 10f;
+    [SerializeField] private Transform attackPoint;
+    [SerializeField] private LayerMask enemyLayers;
+    [SerializeField] private Transform circleVisual;
 
+    private void Start()
+    {
+        circleVisual.localScale = new Vector3(range * 2, range * 2, 1);
+    }
+    
     private void Update()
     {
         if (_fireCountdown <= 0f)
@@ -18,7 +26,11 @@ public class MeleeCircleWeapon : Weapon
 
     private void DoDamage()
     {
-        
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, range, enemyLayers);
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            enemy.GetComponent<Enemy>().TakeDamage(damage);
+        }
     }
     
     public override void Upgrade()
